3a592cad36d95b72b43ba3628f20f1088e712b79
aslamcsebd <aslamcsebd@gmail.com>
2022-09-29 17:41:55 +0600

    Menu title hove done

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -115,9 +115,9 @@
 <script>
     $(document).ready(function(){
 
-      $(document).on('click','.js-menu_toggle.closed',function(e){
+      $(document).on('mouseover','.side_menu',function(e){
          e.preventDefault(); $('.list_load, .list_item').stop();
-         $(this).removeClass('closed').addClass('opened');
+         $('.list_load').removeClass('closed').addClass('opened');
          $('.side_menu').css({ 'right':'0px' });
          var count = $('.list_item').length;
          $('.list_load').slideDown( (count*.6)*100 );
@@ -134,9 +134,9 @@
          });
       });
 
-      $(document).on('click','.js-menu_toggle.opened',function(e){
+      $(document).on('mouseleave','.side_menu',function(e){
          e.preventDefault(); $('.list_load, .list_item').stop();
-         $(this).removeClass('opened').addClass('closed');
+         $('.list_load').removeClass('opened').addClass('closed');
 
          $('.side_menu').css({ 'right':'-250px' });
 
@@ -147,6 +147,7 @@
          });
          $('.list_load').slideUp(300);
       });
+      
    });
 </script>
 
